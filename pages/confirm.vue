21fcff3bb0f49db370e83e6cb86c20b2dcf5beed
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-04-14 16:53:01 -0400

    send logged in user to confirm page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
 const user = useSupabaseUser()
 
-watch(user, () => {
-    if (user.value) {
-        // redirect to protected page
-        return navigateTo('/')
-    }
-}, { immediate: true })
 </script>
 
 <template>
-    <p>Waiting for login ...</p>
+    <p>Hi {{ user?.email }}</p>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -29,11 +29,11 @@ async function logIn() {
             email: email.value,
             password: password.value,
         })
-        console.log('data', data);
-        console.log('error', error);
+        // console.log('data', data);
+        // console.log('error', error);
 
-        // alert('you are logged in!')
-        navigateTo('/')
+        console.log('you are logged in!')
+        navigateTo('/confirm')
 
 
     } catch (error) {
@@ -52,17 +52,47 @@ onMounted(() => {
 
 </script>
 <template>
-    <h1>Login</h1>
-    <form>
-        <label for="email">Type your email</label>
-        <input type="email" v-model="email" id="email">
-        <label for="pwd">Add a password</label><input type="password" name="password" id="pwd" v-model="password">
-        <button @click.prevent="signUp">Sign In with Email</button>
-        <button @click.prevent="logIn">Login</button>
+    <h1>Hello there, please login</h1>
+    <section>
+        <form>
+            <label for="email">Type your email</label>
+            <input type="email" v-model="email" id="email">
+            <label for="pwd">Add a password</label><input type="password" name="password" id="pwd" v-model="password">
+            <div class="btn-container"> <button @click.prevent="signUp">Sign In with Email</button>
+                <button @click.prevent="logIn">Login</button>
+            </div>
 
-        <div class="msgs">
-            <p v-if="successMsg">{{ successMsg }}</p>
-            <p v-else style="color: red">{{ errorrMsg }}</p>
-        </div>
-    </form>
+            <div class="msgs">
+                <p v-if="successMsg">{{ successMsg }}</p>
+                <p v-else style="color: red">{{ errorrMsg }}</p>
+            </div>
+        </form>
+    </section>
 </template>
+<style scoped>
+Section {
+    display: flex;
+    justify-content: center;
+}
+
+form {
+    display: flex;
+    flex-flow: column wrap;
+    width: 30em;
+    justify-content: center;
+    outline: 2px solid #333;
+    padding: 1.5rem 1.25rem;
+    border-radius: 5px;
+
+    .btn-container {
+        display: flex;
+        flex-flow: column wrap;
+        row-gap: 0.5rem;
+        margin-block-start: 0.25rem;
+
+        button {
+            margin-inline: auto;
+        }
+    }
+}
+</style>
